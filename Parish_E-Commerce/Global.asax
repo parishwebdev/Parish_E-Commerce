294b6cc24c8cf3f1428e9313cce951797cb02b33
parishwebdev <parishwebdev@gmail.com>
2019-01-02 20:23:32 -0600

    Add project files.

---
+++ Parish_E-Commerce/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Parish_E_Commerce.MvcApplication" Language="C#" %>
---
+++ Parish_E-Commerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parish_E_Commerce.Startup))]
+namespace Parish_E_Commerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
